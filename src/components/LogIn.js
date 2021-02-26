35bae64e3eda3cad7dd5f180005c29fa9d595feb
ChadDiaz <chadcdiaz@gmail.com>
2021-02-25 22:38:54 -0500

    added actions

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useHistory} from 'react-router-dom'
 
 const LogIn = () => {
+    const [credentials, setCredentials] = useState({
+        user
+    })
     return (
         <div>
             <p>Hello from Login</p>
