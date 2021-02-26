4d73e40308d635b9d80d7f3c1977f85effc579e6
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-02-26 01:33:16 -0500

    as far as I'm getting tonight figuring out the countries dropdown

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axiosWithAuth from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const Center = styled.div`
   display: flex;
@@ -8,17 +9,81 @@ const Center = styled.div`
 `;
 
 const Register = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+    country: {
+      Kenya: false, // doesn't work
+      Uganda: false,
+      Rwanda: false,
+      Tanzania: false,
+      Sudan: false, // doesn't work
+    },
+  });
+
+  const [countries, setCountries] = useState([]);
+  console.log("these are the countries", countries);
+
+  const { push } = useHistory();
+
+  const [error, setError] = useState("");
+
+  const handleChanges = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitRegistration = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/users/register", credentials)
+      .then((response) => {
+        console.log("Registration submit success", response);
+        localStorage.setItem("token", response);
+        push("/login");
+      })
+      .catch((error) => {
+        console.log("Registration error", error.response.data.message); // REVIEW how do you find the error
+        setError(error.response.data.message);
+      });
+  };
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/countries")
+      .then((response) => {
+        console.log("countries success response", response);
+        console.log("figuring out country dropdown", response.data);
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.log("countries error", error);
+      });
+  }, []);
+
   return (
     <Center>
-      <form onSubmit="">
-        <label htmlFor="name" />
+      <form onSubmit={submitRegistration}>
+        {/* <label htmlFor="name" />
         name:<br></br>
         <input
           type="name"
           name="name"
           value=""
-          onChange=""
+          onChange={handleChanges}
           placeholder="name required"
+        />
+        <br></br> */}
+        <label htmlFor="email" />
+        email:<br></br>
+        <input
+          type="email"
+          name="email"
+          value={credentials.email}
+          onChange={handleChanges}
+          placeholder="email required"
         />
         <br></br>
         <label htmlFor="password" />
@@ -26,43 +91,61 @@ const Register = () => {
         <input
           type="password"
           name="password"
-          value=""
-          onChange=""
+          value={credentials.password}
+          onChange={handleChanges}
           placeholder="password required."
         />
         <br></br>
-        <label htmlFor="country_id" />
-        location: <br></br>
-        <input
-          type="text"
-          name="country_id"
-          value=""
-          onChange=""
-          placeholder="country location?"
-        />
-        <br></br>
+        <label htmlFor="country">
+          location:<br></br>
+          <select
+            name="country"
+            value={credentials.country}
+            //   placeholder="country location?"
+            onChange={handleChanges}
+          >
+            <option value="Kenya">Kenya</option>
+            <option value="Uganda">Uganda</option>
+            <option value="Tanzania">Tanzania</option>
+            <option value="Rwanda">Rwanda</option>
+            <option value="Sudan">South Sudan</option>
+            {/* {countries.map((oneCountry) => {
+              <option value={oneCountry.country}>{oneCountry.country}</option>;
+            })} */}
+            {/* <option value={countries.country}>{countries.country}</option> */}
+          </select>
+        </label>
+        {/* <br></br>
         <label htmlFor="user_photo" />
         user photo?<br></br>
         <input
           type="text"
           name="user_photo"
           value=""
-          onChange=""
+          onChange={handleChanges}
           placeholder="user photo?"
         />
         <br></br>
-        <label htmlFor="user_info" />
+        <label htmlFor="user_info" >
         tell us about yourself!<br></br>
-        <input
+        <textarea
           type="text"
           name="user_info"
-          value=""
-          onChange=""
+          value={credentials.user_info}
+          onChange={handleChanges}
           placeholder="bio?"
         />
+        </label>*/}
         <br></br>
         <button style={{ marginTop: "5px" }}>Ready to register?</button>
+        <br></br>
+        {error ? <div style={{ color: "red" }}>{error}</div> : null}
       </form>
+      {/* <div>
+        {countries.map((item) => {
+          <div>{item.country}</div>;
+        })}
+      </div> */}
     </Center>
   );
 };
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const axiosWIthAuth = () => {
+const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://ialkamal-be-amp.herokuapp.com",
+    baseURL: "https://ialkamal-be-amp.herokuapp.com/api",
     headers: {
-      Authorization: token,
+      Authorization: `Bearer ${token}`,
     },
   });
 };
 
-export default axiosWIthAuth;
+export default axiosWithAuth;
