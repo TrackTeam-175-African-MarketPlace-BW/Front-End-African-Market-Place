16547e3c8d11aaaf8fef64303bf5251e199522b2
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-02-26 01:52:48 -0500

    figuring out push error

--- src/components/OwnerProfile.js
+++ src/components/OwnerProfile.js
@@ -1,11 +1,24 @@
-import React from "react"; 
+import React, { useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 // NOTE baseURL/api /${id}/profile
 
 const OwnerProfile = () => {
+  // REVIEW can I only view the user profile if I am currently logged in as that user?
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("users/1")
+      .then((response) => {
+        console.log(("OwnerProfile success response", response));
+      })
+      .catch((error) => {
+        console.log("OwnerProfile error", error.response.data.message);
+      });
+  }, []);
+
   return (
-      <div>
-          
+    <div>
       <p>Hello From Owner Profile!</p>
     </div>
   );
