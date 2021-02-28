f5955aa414bf43de55ae4de7810e508899892fa9
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-02-28 14:24:51 -0500

    got the information rendering properly in OwnerProfile without styling

--- src/actions/ownerActions.js
+++ src/actions/ownerActions.js
@@ -1,34 +1,31 @@
 import axiosWithAuth from "../utils/axiosWithAuth";
 
-
 export const USER_LOADING = "USER_LOADING";
 export const USER_RETRIEVED = "USER_RETRIEVED";
 export const USER_ITEMS_RETRIEVED = "USER_ITEMS_RETRIEVED";
 export const ERROR_LOADING_USER = "ERROR_LOADING_USER";
 
-
-
 export const loadUser = (id) => {
   return (dispatch) => {
-    console.log("ID HERE: ", id);
+    // console.log("ID HERE: ", id);
     dispatch({ type: USER_LOADING });
     axiosWithAuth()
       .get(`/users/${id}`)
       .then((response) => {
-        console.log(
-          "sc: ownerActions: loadUser: axios.get response: ",
-          response
-        );
+        // console.log(
+        //   "sc: ownerActions: loadUser: axios.get response: ",
+        //   response
+        // );
         dispatch({
           type: USER_RETRIEVED,
           payload: response.data,
         });
       })
       .catch((error) => {
-        console.log(
-          "sc: ownerActions: loadUser: axios.get error: ",
-          error.response
-        );
+        // console.log(
+        //   "sc: ownerActions: loadUser: axios.get error: ",
+        //   error.response
+        // );
         dispatch({
           type: ERROR_LOADING_USER,
           payload: error.response.data.message,
@@ -43,20 +40,20 @@ export const loadUserItems = (id) => {
     axiosWithAuth()
       .get(`/users/${id}/items`)
       .then((response) => {
-        console.log(
-          "sc: ownerActions: loadUser: axios.get response: ",
-          response
-        );
+        // console.log(
+        //   "sc: ownerActions: loadUser: axios.get response: ",
+        //   response
+        // );
         dispatch({
           type: USER_ITEMS_RETRIEVED,
           payload: response.data,
         });
       })
       .catch((error) => {
-        console.log(
-          "sc: ownerActions: loadUser: axios.get error: ",
-          error.response
-        );
+        // console.log(
+        //   "sc: ownerActions: loadUser: axios.get error: ",
+        //   error.response
+        // );
         dispatch({
           type: ERROR_LOADING_USER,
           payload: error.response.data.message,
--- src/components/OwnerProfile.js
+++ src/components/OwnerProfile.js
@@ -1,51 +1,81 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axiosWithAuth from "../utils/axiosWithAuth";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { loadUser, loadUserItems } from "../actions/ownerActions";
 
 // NOTE baseURL/api /${id}/profile
 
-const Center = styled.div`
-  display: flex;
-  justify-content: center;
-`;
+// const Center = styled.div`
+//   display: flex;
+//   justify-content: center;
+// `;
 
 const OwnerProfile = (props) => {
-  const { id } = useParams();
+  const { id } = useParams(); // REVIEW pretty sure I will never understand this
+  // console.log("USE PARAMS", useParams());
+  // console.log("sr: ownerProfile.js, const ownerProfile props", props);
 
   useEffect(() => {
     props.loadUser(id);
     props.loadUserItems(id);
-    props.setIsLoggedIn(true);
+    props.setIsLoggedIn(true); // REVIEW where is this coming from
   }, []);
 
+  // console.log("these are the items for sale", props.itemsForSale);
+
   return (
-    <Center>
-      {/* <div>
-        <p>your profile!</p>
-        {props.user_photo ? <div>photo: {props.user_photo}</div> : null}
+    <div>
+      <div>
+        <h1>your profile!</h1>
+        name: {props.ownerProfile.name}
         <br></br>
-        <p>name: {props.name}</p>
+        <img
+          src={props.ownerProfile.user_photo}
+          alt={props.ownerProfile.name}
+        ></img>
         <br></br>
-        <p>email: {props.email}</p>
+        country: {props.ownerProfile.country}
         <br></br>
-        <p>country: {props.country_id}</p>
+        email: {props.ownerProfile.email}
         <br></br>
-        {props.user_info ? <div>bio: {props.user_info}</div> : null}
-      </div> */}
-      <h1>OWNER PROFILE</h1>
-      {JSON.stringify(props.ownerProfile, 2, "")}
-      <img src={props.ownerProfile.user_photo}></img>
-      {JSON.stringify(props.itemsForSale, 2, "")}
+        bio: {props.ownerProfile.user_info}
+        <br></br>
+        {/* {JSON.stringify(props.ownerProfile, 2, "")} */}
+      </div>
+      <div>
+        <h1>items currently for sale:</h1>
+        {props.itemsForSale.map((item) => {
+          return (
+            <div key={item.id}>
+              name: {item.name}
+              <br></br>
+              description: {item.description}
+              <br></br>
+              price: {item.price}
+              <br></br>
+              category: {item.category}
+              <br></br>
+              market: {item.market}
+              <br></br>
+              country: {item.country}
+              <br></br>
+              <br></br>
+            </div>
+          );
+        })}
+        {/* {props.itemsForSale.map((item) => {
+          return <div>{item}</div>;
+        })} */}
+        {/* {JSON.stringify(props.itemsForSale, 2, "")} */}
+      </div>
       {props.error && <p style={{ color: "red" }}>{props.error}</p>}
-    </Center>
+    </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log("STATE: ", state);
+  // console.log("STATE: ", state);
   return {
     owner_id: state.ORS.owner_id,
     ownerProfile: state.ORS.ownerProfile,
