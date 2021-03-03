48c7fea0eed1b555b3033a8bc7458480b9bdff1b
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-03-03 02:19:12 -0500

    basic edit item styling

--- src/components/ITEMS/EditItem.js
+++ src/components/ITEMS/EditItem.js
@@ -1,8 +1,59 @@
+import "../../OwnerProfile.css";
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 import axiosWithAuth from "../../utils/axiosWithAuth";
-import { updateSingleItem } from '../../actions/itemsActions';
+import { updateSingleItem } from "../../actions/itemsActions";
+import styled from "styled-components";
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-transform: lowercase;
+`;
+
+const Span = styled.span`
+  font-size: 4rem;
+`;
+const Button = styled.button`
+  background: #68773c;
+  border-radius: 8px;
+  color: white;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+  padding-top: 7px;
+  padding-bottom: 7px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  border: 0;
+  box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+  border-radius: 8px;
+  width: 80%;
+  text-transform: lowercase;
+`;
+
+const Names = styled.span`
+  font-family: "Homemade Apple", cursive;
+  font-size: 14px;
+`;
+
+const Select = styled.select`
+  outline: 0;
+  background: white;
+  width: 85%;
+  height: 100%;
+  color: black;
+  cursor: pointer;
+  border: 1px solid white;
+  border-radius: 8px;
+  box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+  padding: 8px;
+  text-transform: lowercase;
+`;
 
 const EditItem = (props) => {
   const [item, setItem] = useState({
@@ -62,89 +113,102 @@ const EditItem = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.updateSingleItem(id, itemId, item)
+    props.updateSingleItem(id, itemId, item);
     push(`/${id}/ownerProfile`);
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name" /> Edit Item Name
-      <input
-        id="name"
-        type="text"
-        name="name"
-        value={item.name}
-        onChange={onChange}
-      />
-      <label htmlFor="description" /> Edit Item Description
-      <input
-        id="description"
-        type="text"
-        name="description"
-        value={item.description}
-        onChange={onChange}
-      />
-      <label htmlFor="price" /> Edit Item Price
-      <input
-        id="price"
-        type="text"
-        name="price"
-        value={item.price}
-        onChange={onChange}
-      />
-      <label htmlFor="category" /> Edit Item Category
-      <select
-        id="category"
-        name="category"
-        value={item.category}
-        onChange={onChange}
-        defaultValue="pickOne"
-      >
-        <option value="pickOne">--- Pick One ---</option>
-        {categories.map((category) => {
-          return (
-            <option key={category.id} value={category.category}>
-              {category.category}
-            </option>
-          );
-        })}
-      </select>
-      <label htmlFor="market" /> Edit Market Location
-      <select
-        id="market"
-        name="market"
-        value={item.market}
-        onChange={onChange}
-        defaultValue="pickOne"
-      >
-        <option value="pickOne">--- Pick One ---</option>
-        {markets.map((market) => {
-          return (
-            <option key={market.id} value={market.market}>
-              {market.market}
-            </option>
-          );
-        })}
-      </select>
-      <label htmlFor="country" /> Edit Country Location
-      <select
-        id="country"
-        name="country"
-        value={item.country}
-        onChange={onChange}
-        defaultValue="pickOne"
-      >
-        <option value="pickOne">--- Pick One ---</option>
-        {countries.map((country) => {
-          return (
-            <option key={country.id} value={country.country}>
-              {country.country} 
-            </option>
-          );
-        })}
-      </select>
-      <button type='submit'>Update Item</button>
-    </form>
+    <Center>
+      <Span role="img" aria-label="corn and vase emoji">
+        🌾🏺🌾
+      </Span>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name" /> <Names>Edit Item Name</Names>:<br></br>
+        <Input
+          id="name"
+          type="text"
+          name="name"
+          value={item.name}
+          onChange={onChange}
+        />
+        <br></br>
+        <label htmlFor="description" /> <Names>Edit Item Description</Names>:
+        <Input
+          id="description"
+          type="text"
+          name="description"
+          value={item.description}
+          onChange={onChange}
+        />
+        <br></br>
+        <label htmlFor="price" /> <Names>Edit Item Price</Names>:<br></br>
+        <Input
+          id="price"
+          type="text"
+          name="price"
+          value={item.price}
+          onChange={onChange}
+        />
+        <br></br>
+        <label htmlFor="category" /> <Names>Edit Item Category</Names>:<br></br>
+        <Select
+          id="category"
+          name="category"
+          value={item.category}
+          onChange={onChange}
+          defaultValue="pickOne"
+        >
+          <option value="pickOne">--- Pick One ---</option>
+          {categories.map((category) => {
+            return (
+              <option key={category.id} value={category.category}>
+                {category.category}
+              </option>
+            );
+          })}
+        </Select>
+        <br></br>
+        <label htmlFor="market" /> <Names>Edit Market Location</Names>:<br></br>
+        <Select
+          id="market"
+          name="market"
+          value={item.market}
+          onChange={onChange}
+          defaultValue="pickOne"
+        >
+          <option value="pickOne">--- Pick One ---</option>
+          {markets.map((market) => {
+            return (
+              <option key={market.id} value={market.market}>
+                {market.market}
+              </option>
+            );
+          })}
+        </Select>
+        <br></br>
+        <label htmlFor="country" /> <Names>Edit Country Location</Names>:
+        <br></br>
+        <Select
+          id="country"
+          name="country"
+          value={item.country}
+          onChange={onChange}
+          defaultValue="pickOne"
+        >
+          <option value="pickOne">--- Pick One ---</option>
+          {countries.map((country) => {
+            return (
+              <option key={country.id} value={country.country}>
+                {country.country}
+              </option>
+            );
+          })}
+        </Select>
+        <br></br>
+        <Button type="submit">update item</Button>
+      </form>
+    </Center>
   );
 };
 
@@ -156,4 +220,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {updateSingleItem})(EditItem);
+export default connect(mapStateToProps, { updateSingleItem })(EditItem);
