6a47941bf5755ad5443acbd40625d2e21e3c609e
Victoria Trac <victoriatrac@gmail.com>
2021-03-03 22:27:09 -0700

    added functionality to form and account authorization

--- src/components/FakeLoginForm.js
+++ src/components/FakeLoginForm.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
 import styled from 'styled-components'
+import axiosWithAuth from '../utils/axiosWithAuth'
+// import { updatedPassword } from '../actions/ownerActions'
 
 const Form = styled.form`
     display: block;
@@ -10,37 +13,88 @@ const Label = styled.label`
     padding-right: 2em;
 `
 
-// const 
+const FormSection = styled.span`
+    padding-right: .5em;
+`
+
+const Button = styled.button`
+    background-color: rgb(165, 70, 35);
+    color: white;
+    border: 1px solid black;
+    padding-left: 1em;
+    padding-right: 1em;
+`
+
+const Error = styled.span`
+    padding-left: .5em;
+    color: red;
+`
 
 const FakeLogIn = () => {
+
+    const [ credentials, setCredentials ] = useState({
+        email: '',
+        password: '',        
+    })
+    const [ error, setError ] = useState("")
+    const { push } = useHistory()
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        axiosWithAuth()
+            .post('/users/login', credentials)
+            .then((res) => {
+                localStorage.setItem('token', res.data.token)
+                push(`${res.data.user.id}/ownerProfile`)
+            })
+            .catch((err) => {
+                console.log(
+                    setError(err.response.data.message)
+                )
+            })
+    }
+
     return (
-        <Form>
-            <Label>
-                Email:
+        <Form onSubmit={handleLogin}>
+            <Label htmlFor='email'>
+                <FormSection>Email:</FormSection>
                 <input 
                     id='email'
                     name='email'
                     type='email'
-                    // value=
+                    value={credentials.email}
                     placeholder='enter your login email'
-                    // onChange={handleChange}
+                    onChange={handleChange}
                 />
             </Label>
-            <Label>
-                Password:
+            <Label htmlFor='password'>
+            <FormSection>Password:</FormSection>
                 <input 
                     id='password'
                     name='password'
-                    // value={}
-                    // onChange={handleChange}
+                    type='password'
+                    value={credentials.password}
+                    onChange={handleChange}
                     placeholder='enter your password'
                 />
             </Label>
-            <button 
+            <Button 
                 type="submit"
             >
                 log in
-            </button>
+            </Button>
+            {error && (
+                <Error>
+                    {error} please try again or{" "}<NavLink to="/register">make an account</NavLink>
+                </Error>
+            )}
             {/* <label>
                 Email:
                 <Input 
@@ -51,4 +105,4 @@ const FakeLogIn = () => {
     )
 }
 
-export default FakeLogIn;
+export default FakeLogIn
