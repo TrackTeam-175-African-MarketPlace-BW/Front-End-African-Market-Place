7d5e790010232e808a459a693719222a3d542798
Victoria Trac <victoriatrac@gmail.com>
2021-03-06 18:34:36 -0700

    finished landing page and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,29 +4,33 @@ import styled from "styled-components";
 
 const FooterStyle = styled.div`
     height: auto;
-    padding: 2rem 2rem 4rem 2rem;
+    padding: 2rem 0rem;
+    text-align: center;
+    width: 100%;
 `;
 
-const NavDiv = styled.div`
+const FooterDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 500px;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const Footer =  ({ isLoggedIn }) => {
     return (
         <FooterStyle>
-            <center>
-                <h3>footer footer footer</h3>
-                <NavDiv>
+            {/* <center> */}
+                {/* <h1>footer footer footer</h1> */}
+                <FooterDiv>
+                    <Link to="/">Home</Link>
                     {isLoggedIn ? <></> : <Link to="/login">Current Users: Log In</Link>}
                     {isLoggedIn ? <></> : <Link to="/register">Register Here</Link>}
                     <Link to="/team">Meet The BW Team</Link>
-                    <Link to="/">Home</Link>
                     {isLoggedIn && <Link to="/:id/ownerProfile">Your Profile</Link>}
                     {isLoggedIn && <Link to="/:id/itemsList">Items List</Link>}
-                </NavDiv>
-            </center>
+                </FooterDiv>
+            {/* </center> */}
         </FooterStyle>
     );
 };
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../LandingPage.css';
 import backgroundImage1 from '../images/food_1.jpg';
 import backgroundImage2 from '../images/food_2.jpg';
+import storyImage1 from '../images/story_1.jpg';
+import storyImage2 from '../images/story_2.jpg';
+import storyImage3 from '../images/story_3.jpg';
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
@@ -14,8 +17,8 @@ export default function LandingPage() {
                     className="div-bg-img"
                 />
                 <div className="flex-container div-1-flex">
-                    <div className="div-1-text-box">
-                        <h2>Sauti African Market Place empowers small business owners, particularly women, to improve their business and economic opportunities to grow out of poverty.</h2>
+                    <div className="text-box div-1-text-box">
+                        <h2 className="div-1-text">Sauti African Market Place empowers small business owners, particularly women, to improve their business and economic opportunities to grow out of poverty.</h2>
                         <div className="flex-container btn-div-flex">
                             <Link
                                 to="/"
@@ -40,14 +43,17 @@ export default function LandingPage() {
                 </div>
             </div>
             <div className="flex-container text-div div-container div-2">
-                <h2>Content about what we do</h2>
-                <p>Good man. Nixon's pro-war and pro-family. Say it in Russian! I'm Santa Claus! Why did you bring us here?
-                WINDMILLS DO NOT WORK THAT WAY! GOOD NIGHT! This is the worst kind of discrimination: the kind against me! I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness.
-                That's right, baby. I ain't your loverboy Flexo, the guy you love so much. You even love anyone pretending to be him! With a warning label this big, you know they gotta be fun! I'll tell them you went down prying the wedding ring off his cold, dead finger.
-                Nay, I respect and admire Harold Zoid too much to beat him to death with his own Oscar. What are you hacking off? Is it my torso?! 'It is!' My precious torso! Okay, it's 500 dollars, you have no choice of carrier, the battery can't hold the charge and the reception isn't very…
-                Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! Yes. You gave me a dollar and some candy. I just want to talk. It has nothing to do with mating. Fry, that doesn't make sense.
-                Noooooo! Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? Who am I making this out to? Negative, bossy meat creature! Do a flip!
-                Ooh, name it after me! No, she'll probably make me do it. Guess again. Okay, I like a challenge. You don't know how to do any of those.</p>
+                <h2>What We Do</h2>
+                <p className="p-fix">Sauti Africa works to provide African farmers and food vendors a platform on which they can network with other small business owners to better position their place in local and surrounding markets. Our goal is to create an accessible way for hard-working people in disadvantaged countries to better their lives.</p>
+                <p>With a free Sauti Africa account, farmers and vendors can:</p>
+                <list>
+                    <li>View current offerings at markets outside of their local area, along with prices, descriptions, and reviews</li>
+                    <li>Meet and build relationships with other small business owners</li>
+                    <li>Discover new markets at which they can vend products</li>
+                    <li>Connect with better opportunities to advance their business</li>
+                    <li>Improve their pricing and marketing strategies</li>
+                </list>
+                <p>Our low-cost strategizing services can help advance businesses even further by providing affordable access to marketing, pricing, and business management specialists.</p>
             </div>
             <div className="div-container img-div div-3">
                 <img
@@ -57,16 +63,34 @@ export default function LandingPage() {
                 />
             </div>
             <div className="flex-container text-div div-container div-4">
-                <h2>Content about success stories</h2>
-                <div className="flex-text">
-                    <p>Good man. Nixon's pro-war and pro-family. Say it in Russian! I'm Santa Claus! Why did you bring us here?
-                    WINDMILLS DO NOT WORK THAT WAY! GOOD NIGHT! This is the worst kind of discrimination: the kind against me! I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness.
-                    That's right, baby. I ain't your loverboy Flexo, the guy you love so much. You even love anyone pretending to be him! With a warning label this big, you know they gotta be fun!I'll tell them you went down prying the wedding ring off his cold, dead finger.
-                    Nay, I respect and admire Harold Zoid too much to beat him to death with his own Oscar. </p>
-                    <p> What are you hacking off? Is it my torso?! 'It is!' My precious torso! Okay, it's 500 dollars, you have no choice of carrier, the battery can't hold the charge and the reception isn't very…
-                    Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! Yes. You gave me a dollar and some candy. I just want to talk. It has nothing to do with mating. Fry, that doesn't make sense.
-                    Noooooo! Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? Who am I making this out to? Negative, bossy meat creature! Do a flip!
-                    Ooh, name it after me! No, she'll probably make me do it. Guess again. Okay, I like a challenge. You don't know how to do any of those.</p>
+                <h2>We've helped small business owners across Africa get closer to their full potential.</h2>
+                <div className="div-4-flex">
+                    <div className="profile">
+                        <img src={storyImage1} />
+                        <h4>Adah, Nigeria</h4>
+                        <p>After receiving an annual grant from Sauti Africa, Adah was able to hire a small crew to help run her fishing business. Having extra hands allowed her to more than double the amount of food she provided to her region, in addition to creating jobs in the community.</p>
+                    </div>
+                    <div className="profile">
+                        <img src={storyImage2} />
+                        <h4>Kwame, Ghana</h4>
+                        <p>Through Sauti Africa's network, Kwame was able to discover new markets at which he could sell his crops. With the additional income, he was able to expand his farm and began growing pineapple, tomatoes, and bananas.</p>
+                    </div>
+                    <div className="profile">
+                        <img src={storyImage3} />
+                        <h4>Nabulungi, Uganda</h4>
+                        <p>When her farm business became stagnant, Nabulungi created new local connections via Sauti Africa and was able to find a source for passionflower seeds. As the only passionflower farmer in her area, she has turned her business around.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-container text-div div-container div-5">
+                <h2 className="div-5-text">With our platform, we hope to encourage economic growth in less fortunate areas of Africa in order to help better lives through increased opportunity.</h2>
+                <div className="div-5-list-container">
+                    <list>
+                        <p>Some of our contributions back into the community:</p>
+                        <li>Annual grants for up-and-coming businesses into our strategizing program</li>
+                        <li>Donations into educational programs for girls' schools in Africa</li>
+                        <li>Financial assistance and short-term, interest free loans on a rolling, bi-monthly basis</li>
+                    </list>
                 </div>
             </div>
         </div>
