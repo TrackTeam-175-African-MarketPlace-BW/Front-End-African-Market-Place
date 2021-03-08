aee0f789d931e2e8d6f7e0512f5bdc9bf691a6f0
ChadDiaz <chadcdiaz@gmail.com>
2021-03-08 17:01:31 -0500

    deleted the shoppingcart reducer from the index

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from "redux";
 import { itemsReducer as IRS } from "./itemsReducer";
 import { ownerReducer as ORS } from "./ownerReducer";
-import { shoppingCartReducer as SCR } from "./shoppingCartReducer";
+
 
 export default combineReducers({
   IRS,
   ORS,
-  SCR,
 });
